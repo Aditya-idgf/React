bcfbdbd04a5b8adef31f2188f47c2d1a771c60dd
Aditya <108931178+Aditya-idgf@users.noreply.github.com>
2026-06-19 20:14:30 +0530

    Code updated

---
+++ 08_ReactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
